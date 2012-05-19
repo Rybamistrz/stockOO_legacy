cca9da7097374796639fcd4162325364a9185370
Paweł Okoń <pawel@okon.eu>
2012-05-19 13:59:02 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StockOO::Application.config.secret_token = '02ad2c82796a2f98da75d7a5cc66120679f4720b65979e5cf7a9e671f4c93116bcd4fe62b9b08a9814bbbbda01270e1d57aa3472a2c1bf69755735ccbfb645ea'
