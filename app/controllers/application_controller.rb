55c0b440600aba362ea5392fdd9f0580479c2e95
Paweł Okoń <pawel@okon.eu>
2012-05-20 23:16:10 +0200

    added creation of super admin in seed and set user_id for product

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  helper_method :current_admin
+
+  def current_admin
+    @super_admin ||= User.where(:role => "super_admin").first
+  end
+
 end
