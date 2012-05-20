243a825b2aa71df24d4e77a2176babab42ed7eed
Paweł Okoń <pawel@okon.eu>
2012-05-20 20:36:58 +0200

    added parsial for manufacturer, created sidebar option for manufacturer

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,9 +12,11 @@ module ApplicationHelper
   def sidebar_options(view_name)
     case view_name
     when :users_index 
-      [{ :url => new_user_path, :icon => "icon-plus", :label => t("sidebar_options.new_user") }]
+      [{ :url => new_user_path, :icon => "icon-plus", :label => t("views.users_index.add_user") }]
     when :categories_index
-      [{ :url => new_category_path, :icon => "icon-plus", :label => t("sidebar_options.new_category") }]
-    end
+      [{ :url => new_category_path, :icon => "icon-plus", :label => t("views.categories_index.add_category") }]
+    when :manufacturers_index
+      [{ :url => new_manufacturer_path, :icon => "icon-plus", :label => t("views.manufacturers_index.add_manufacturer") }]
+    end 
   end
 end
