cdb88f316106363c3895549d0fbaff02d02a1773
Paweł Okoń <pawel@okon.eu>
2012-05-20 23:48:28 +0200

    added basic stats in home#index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
   def index
+    @products = Product.where("created_at >= :start_date", :start_date => Time.now.midnight).order("created_at DESC")
+    @categories = Category.where("created_at >= :start_date", :start_date => Time.now.midnight).order("created_at DESC")
+    @manufacturers = Manufacturer.where("created_at >= :start_date", :start_date => Time.now.midnight).order("created_at DESC")
   end
 end
