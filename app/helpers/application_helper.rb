27632d37df5830cb0f927e69b89ef86838a5df86
Paweł Okoń <pawel@okon.eu>
2012-05-20 17:52:15 +0200

    created polish locale, added acvive section on navi bar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def main_menu
+    [
+      { :url => root_path, :label => t("menu_labels.home") },
+      { :url => users_path, :label => t("menu_labels.users") },
+      { :url => categories_path, :label => t("menu_labels.categories") },
+      { :url => manufacturers_path, :label => t("menu_labels.manufacturers") },
+      { :url => products_path, :label => t("menu_labels.products") }
+    ]
+  end
 end
